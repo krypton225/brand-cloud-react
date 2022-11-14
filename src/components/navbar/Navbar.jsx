7bdae6f9cf8a3cb1b0c450edfba497cbdc2ed735
krypton225 <k.mohamed.11.98@gmail.com>
2022-11-14 09:04:15 +0200

    ADD main util @ sass

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,40 @@
 const Navbar = () => {
     return (
-        <p>Khaled</p>
+        <nav className="navbar" id="navbar">
+            <div className="container">
+                <div className="row">
+                    <h1 className="navbar__logo">brand cloud</h1>
+
+                    <ul className="navbar__list">
+                        <li className="navbar__list__item">
+                            <a href="/">home</a>
+                        </li>
+
+                        <li className="navbar__list__item">
+                            <a href="/">about</a>
+                        </li>
+
+                        <li className="navbar__list__item">
+                            <a href="/">support</a>
+                        </li>
+
+                        <li className="navbar__list__item">
+                            <a href="/">platforms</a>
+                        </li>
+
+                        <li className="navbar__list__item">
+                            <a href="/">pricing</a>
+                        </li>
+                    </ul>
+
+                    <div className="navbar__toggler">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+            </div>
+        </nav>
     )
 }
 
