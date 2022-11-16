e9057ba1755efaa00d42ae8902bec016af66c735
krypton225 <k.mohamed.11.98@gmail.com>
2022-11-16 20:23:46 +0200

    ADD FormSubmit comp

--- src/components/footer/FormFooter.jsx
+++ src/components/footer/FormFooter.jsx
@@ -1,11 +1,12 @@
 import FormInput from "./FormInput";
+import FormSubmit from "./FormSubmit";
 
 const FormFooter = () => {
     return (
         <form className="footer__col__form" autoComplete='off'>
             <div className='footer__col__form__container'>
                 <FormInput inputType="email" inputName="email" myPlaceHolder="enter your email" completeInType={false} />
-                <input type="submit" className='footer__col__form__container__submit' value="subscribe" />
+                <FormSubmit valueText={"subscribe"} />
             </div>
         </form>
     )
---
+++ src/components/footer/FormSubmit.jsx
@@ -0,0 +1,7 @@
+const FormSubmit = ({ valueText }) => {
+    return (
+        <input type="submit" className='footer__col__form__container__submit' value={valueText} />
+    )
+}
+
+export default FormSubmit;
